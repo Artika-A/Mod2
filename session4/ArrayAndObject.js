afdfc66147fd894851d1e53f3e682b48399720ec
Artika-A <artikaafaisal@gmail.com>
2025-06-13 12:58:05 +0800

    Arrays, Objects, Functions, Scope, DOM

---
+++ session4/ArrayAndObject.js
@@ -0,0 +1,81 @@
+let fruits = ["Grapes", "Strawberry","Pear","Apple"]
+//console.log(fruits)
+//console.log(fruits[0])
+
+//before push
+//console.log(fruits[fruits.length - 1])
+
+//push - add an element at the end of the array
+//fruits("Orange")
+
+//after push
+//console.log(fruits.length[fruits.length] - 1)
+
+//pop - remove the last element from the array
+//fruits.pop()
+
+//after pop
+//console.log(fruits[fruits.length - 1])
+
+//splice- remove element at position followed by movement to the right
+//fruits.splice(0, 1)
+
+//after splice
+//console.log(fruits)
+
+//fruits.forEach(function(fruit, i){
+    //console.log(fruit)
+    //console.log(i)
+//})
+
+fruits.forEach(function(fruit, i){
+    console.log("fruit at position", i, "is", fruit)
+   
+})
+
+for(let [i, fruit] of fruits.entries){
+    console.log(fruit)
+}
+
+console.log("=================================")
+
+//Objects
+//Objects are just arrays with an identifier for their index
+let myProfile = {
+    name: "Artika",
+    age: 27,
+    email: "artikaafaisal@gmail.com",
+    hobbies: ["reading","painting", "gaming"]
+}
+console.log(myProfile.name)
+console.log(myProfile["name"])
+
+//age
+console.log(myProfile.age)
+
+//1st hobby
+console.log(myProfile.hobbies[0])
+
+
+
+//Mini activity:
+
+// 1. 5 favourite movies
+let favMovies = ["Camp Rock", "Hannah Montana", "High School Musical", "Whisper of the Heart 1995 ", "Spirited Away"]
+
+//2. first and last movies
+
+console.log(favMovies[0])
+console.log(favMovies[favMovies.length - 1])
+
+// 3. Use push() to add one, and pop() to remove one
+favMovies.push("Ponyo")
+favMovies.pop()
+
+let newProfile = {
+    name: "Artika",
+    age: 27,
+    movies: favMovies
+}
+
+console.log(newProfile.movies[1])
---
+++ session4/FunctionAndScope.js
@@ -0,0 +1,103 @@
+//let this_is_name //snake_case
+//let thisIsMyName //camelCase
+//let thisIsMyName  //Caps for words
+
+// Function name | Argument / variable
+function sayHello (helloName){
+    console.log("Hello " + helloName)
+}
+
+sayHello("John")
+
+function addTwoNumbers(num1, num2){
+    //console.log(addTwoNumbers) <-----recursive function
+    //console.log(num1 + num2)
+    return num1 + num2
+}
+
+let sum = addTwoNumbers(2, 3)
+//console.log(sum)
+
+//can you do console.log(addTwoNumbers(3,3))??
+console.log(addTwoNumbers(3,3))
+
+//mini exercise
+//Write a function for subtraction and subtract 7 from 10 (use the return keyword)
+
+
+function subtractTwoNumbers(num1,num2){
+    return num1 -num2
+}
+
+console.log(subtractTwoNumbers(7,10))
+
+
+// Function Expression
+const multiply = function(num1,num2){
+    return num1 * num2
+}
+console.log(multiply(2,2))
+
+//Mini Exercise
+//Use function expression to do divide.
+//Divide 25 by 5
+
+const divide = function(num1,num2){
+    return num1 / num2
+}
+console.log(divide(25, 5))
+
+//Arrow functions
+// shorthand arrow function - can be done in one line
+let myFunction = (a, b) => a * b;
+
+// What is scope??
+//accessibility of variable or arguments
+
+//Global scope
+let Global ="Outside (Global) scope"
+
+let functionScope = () => {
+    let insideFunction = "Inside (Function) scope";
+    //console.log(global)
+    //console.log(insideFunctionScope)
+}
+
+functionScope()
+ console.log(Global);
+ //console.log(InsideFunctionScope)
+
+ //let tryAccessOtherFunctionVar = (){
+ //   console.log(insideFunctionScope)
+ //}
+
+ //Mini Activity
+ //1. create a function that square a number.
+ //2. create a function that returns true if a number is even.
+
+ //Partial Answers
+ // num * num
+ // Either write an if else statement, OR use %
+ // for % method, this is what you need
+ // num % 2 == 0
+
+ 
+ function isOdd(num){
+    return num % 2 == 1
+ }
+
+ console.log(isOdd(5))
+
+
+ let square = (num) => num * num;
+ console.log(square(3))
+
+ //Ternary Operator
+ //Condition ? True case : False case
+ // 5 < 3 ? " num is smaller than 3" : "num is not smaller than 3"
+ let isEven = (num) => num % 2 == 0 ? num + " is Even" : num + " is Odd";
+ isEven = (num) => {
+    return num % 2 == 0 ? num + " is Even" : num + " is Odd";
+ }
+ console.log(isEven(8)) // True
+ console.log(isEven(9)) // false
